956eabb57877ac4caf58c896de4705fd3f9a4b0b
Alex Sandro Belcastro <alex.belcastro@emc.com>
2016-12-22 02:48:36 -0200

    Approach on how to read the data file has changed to read line-by-line to avoid full scan to compute and out of memory in case of data file larger than memory in computing machine. Version has been rolled up.

--- src/main/scala/com/belcastro/aleques/Challenge.scala
+++ src/main/scala/com/belcastro/aleques/Challenge.scala
@@ -1,21 +1,23 @@
 package com.belcastro.aleques
 
 import java.io.File
-import java.time.{LocalDateTime, ZoneOffset}
 
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object Challenge {
 
-  val SPACE = " "
-  val TAB   = "\t"
+  val SPACE  = " "
+  val TAB    = "\t"
   val HYPHEN = "-"
+
+  val TIME_WINDOW_LENGTH: Int = 60
 
   def main(args: Array[String]): Unit = {
     usage(args)
-    val dataProcessed = process(getFileContentParsed(args(0)))
     println(getHeader)
-    dataProcessed.foreach(item => println(toOutput(item)))
+    processData(args(0))
   }
 
   def usage(args: Array[String]): Unit = {
@@ -24,51 +26,10 @@ object Challenge {
         <TimeSerieFilePath> - Absolute path for time series file
         i.e. java -jar challenge-01.00.00.jar /tmp/data_scala.txt
       """)
-  }
 
-  def getFileContentParsed(path: String): List[(Long, Double)] = {
-    val timeSeriesFile = new File(path)
+    val timeSeriesFile = new File(args(0))
     require(timeSeriesFile.exists,
       s"Path: ${timeSeriesFile.getAbsolutePath} does not exist. Please provide a valid path for a time series file.")
-
-    Source
-      .fromFile(timeSeriesFile)
-      .getLines
-      .map(line => {
-          val columns = line.replace(TAB, SPACE).split(SPACE, -1)
-          (columns(0).toLong,columns(1).toDouble)
-        }).toList
-
-  }
-
-  def process(data: List[(Long, Double)]): Iterable[(Long, Double, Int, Double, Double, Double)] = {
-    data.groupBy(timeWindow).map(toReport)
-  }
-
-  def timeWindow:((Long, Double)) => Long = {
-    case (epoch, priceRatio) =>
-      LocalDateTime
-        .ofEpochSecond(epoch, 0, ZoneOffset.UTC)
-        .withSecond(0)
-        .toEpochSecond(ZoneOffset.UTC)
-
-  }
-
-  def toReport:((Long, List[(Long, Double)])) => (Long, Double, Int, Double, Double, Double) = {
-    case (timeWindow, measurements) =>
-      val firstRatio = measurements.head._2
-      val initial = (timeWindow, firstRatio, measurements.size, 0.0, firstRatio, firstRatio)
-      measurements.foldLeft(initial) {
-        case ((tw, fpr, s, rs, min, max), measurement) =>
-          val ratio = measurement._2
-          (tw, fpr, s, rs + ratio, math.min(min, ratio), math.max(max, ratio))
-      }
-  }
-
-  def toOutput:((Long, Double, Int, Double, Double, Double)) => String = {
-    case ((timeWindow, priceRatio, num, sum, min, max)) =>
-      val sumRounded = BigDecimal(sum).setScale(5, BigDecimal.RoundingMode.HALF_UP)
-      f"$timeWindow$SPACE$priceRatio$SPACE$num%-3s$SPACE$sumRounded%-10s$SPACE$min$SPACE$max"
   }
 
   def getHeader: String = {
@@ -78,7 +39,62 @@ object Challenge {
     val RS = "RS"
     val MIN_V = "MinV"
     val MAX_V = "MaxV"
-    f"$T%-11s$V%-8s$N%-4s$RS%-11s${MIN_V}%-8s${MAX_V}%-7s%n${HYPHEN * 50}"
+    f"$T%-11s$V%-8s$N%-4s$RS%-11s$MIN_V%-8s$MAX_V%-7s%n${HYPHEN * 50}"
+  }
+
+  def parse(line: String): (Long, Double) = {
+    val columns = line.replace(TAB, SPACE).split(SPACE, -1)
+    (columns(0).toLong, columns(1).toDouble)
+  }
+
+  def anotherTimeWindow(threshold: Long, measurement:(Long, Double)): Boolean = {
+    measurement._1 - TIME_WINDOW_LENGTH >= threshold
+  }
+
+  def beginTimeWindow:((Long, Double)) => Long = {
+    case (epoch, _) =>
+      if (epoch % TIME_WINDOW_LENGTH == 0) epoch
+      else epoch - (epoch % TIME_WINDOW_LENGTH)
+  }
+
+  def toOutput(record: (Long, Double, Int, Double, Double, Double)): String = {
+      val sumRounded = BigDecimal(record._4).setScale(5, BigDecimal.RoundingMode.HALF_UP)
+      f"${record._1}$SPACE${record._2}$SPACE${record._3}%-3s$SPACE$sumRounded%-10s$SPACE${record._5}$SPACE${record._6}"
+  }
+
+  def consolidate(measurements: ListBuffer[(Long, Double)]): (Long, Double, Int, Double, Double, Double) = {
+    (beginTimeWindow(measurements.head),
+      measurements.head._2,
+      measurements.length,
+      measurements.map(_._2).sum,
+      measurements.map(_._2).min,
+      measurements.map(_._2).max)
+  }
+
+  def processData(path: String): Unit = {
+    val data: Iterator[String] = Source.fromFile(path).getLines
+    if ( data.hasNext ) {
+
+      val firstMeasurement = parse(data.next)
+      var timeWindowThreshold = beginTimeWindow(firstMeasurement)
+      var timeWindowData = ListBuffer.empty[(Long, Double)]
+
+      @tailrec
+      def processTimeWindow(measurement: (Long, Double)): Unit = {
+
+        if (anotherTimeWindow(timeWindowThreshold, measurement) && timeWindowData.nonEmpty) {
+          println(toOutput(consolidate(timeWindowData)))
+          timeWindowThreshold = beginTimeWindow(measurement)
+          timeWindowData.clear
+        }
+        timeWindowData += measurement
+
+        if (data.hasNext) processTimeWindow(parse(data.next))
+        else consolidate(timeWindowData)
+      }
+
+      processTimeWindow(firstMeasurement)
+    }
   }
 
 }
--- src/test/scala/com/belcastro/aleques/ChallengeTest.scala
+++ src/test/scala/com/belcastro/aleques/ChallengeTest.scala
@@ -1,85 +1,72 @@
 package com.belcastro.aleques
 
-import org.junit.Assert
-import org.junit.Test
-import Challenge._
+import com.belcastro.aleques.Challenge._
+import org.junit.{Assert, Test}
+
+import scala.collection.mutable.ListBuffer
 
 class ChallengeTest {
 
   val manyArgs: Array[String] = Array("/tmp/dummy.txt","AnotherArgument")
   val noArg: Array[String] = Array.empty
   val arg: Array[String] = Array("/tmp/dummy.txt")
-  val testFile: String = "src/test/resources/data_scala.txt"
 
   @Test(expected = classOf[IllegalArgumentException])
-  def noArgumentUsageTest: Unit = {
-    usage(noArg)
-  }
-
-  @Test
-  def usageTest: Unit = {
-    usage(arg)
-  }
+  def noArgumentUsageTest: Unit = usage(noArg)
 
   @Test(expected = classOf[IllegalArgumentException])
-  def manyArgumentsUsageTest: Unit = {
-    usage(manyArgs)
-  }
+  def usageTest: Unit = usage(arg)
 
   @Test(expected = classOf[IllegalArgumentException])
-  def noFileGetFileContentParsedTest: Unit = {
-    getFileContentParsed("dummy.txt")
+  def manyArgumentsUsageTest: Unit = usage(manyArgs)
+
+  @Test
+  def getHeaderTest: Unit = {
+    val headerExpected = "T          S       N   RS         MinV    MaxV   " +
+      "\n--------------------------------------------------"
+
+    Assert.assertEquals(headerExpected, getHeader)
   }
 
   @Test
-  def getFileContentParsedTest: Unit = {
-    val data = getFileContentParsed(testFile)
-    Assert.assertTrue(data.size == 49)
+  def parseTest: Unit = {
+    val line : String = "1355270609 1.80215"
+    val line1: String = "1355270621 1.80185"
+    val line2: String = "1355270646 1.80195"
+    Assert.assertEquals((1355270609l,1.80215d), parse(line))
+    Assert.assertEquals((1355270621l,1.80185d), parse(line1))
+    Assert.assertEquals((1355270646l,1.80195d), parse(line2))
   }
 
   @Test
-  def timeWindowEndTest: Unit = {
-    val tuple :(Long, Double) = (1355270609, 1.80215)
-    val tuple1:(Long, Double) = (1355270621, 1.80185)
-    val tuple2:(Long, Double) = (1355270646, 1.80195)
-    Assert.assertEquals(1355270580, timeWindow(tuple))
-    Assert.assertEquals(1355270580, timeWindow(tuple1))
-    Assert.assertEquals(1355270640, timeWindow(tuple2))
+  def anotherTimeWindowTest: Unit = {
+    val timeWindowData:List[(Long, Double)] = List((1355270580,0.0),(1355270601,0.0),(1355270639,0.0))
+    timeWindowData.foreach(e => Assert.assertEquals(false, anotherTimeWindow(1355270580l, e)))
+    Assert.assertEquals(true, anotherTimeWindow(1355270580l,(1355270640l,0.0)))
   }
 
   @Test
-  def toReportTest: Unit = {
-    val tuple : (Long, List[(Long, Double)]) = (1355270640, List((1355270609L, 1.80215)))
-    val tuple2: (Long, List[(Long, Double)]) = (1355270700, List((1355270609L, 1.80215), (1355270621, 1.80185)))
-    val tuple3: (Long, List[(Long, Double)]) = (1355270580, List((1355270609L, 1.80215), (1355270621, 1.80185), (1355270646, 1.80195)))
-    Assert.assertEquals((1355270640, 1.80215, 1, 1.80215, 1.80215, 1.80215), toReport(tuple))
-    Assert.assertEquals((1355270700, 1.80215, 2, 3.604  , 1.80185, 1.80215), toReport(tuple2))
-    Assert.assertEquals((1355270580, 1.80215, 3, 5.40595, 1.80185, 1.80215), toReport(tuple3))
+  def beginTimeWindowTest: Unit = {
+    val timeWindowData:List[(Long, Double)] = List((1355270580,0.0),(1355270601,0.0),(1355270639,0.0))
+    timeWindowData.foreach(e => Assert.assertEquals(1355270580l, beginTimeWindow(e)))
   }
 
   @Test
-  def processTest: Unit = {
+  def consolidateTest: Unit = {
+    val data : ListBuffer[(Long, Double)] = ListBuffer((1355270580,1.80215), (1355270601,1.80185), (1355270639,1.80195))
+    val data1: ListBuffer[(Long, Double)] = ListBuffer((1355270645,1.80225), (1355270690,1.80215))
+    val data2: ListBuffer[(Long, Double)] = ListBuffer((1355270703,1.80235), (1355270704,1.80205))
+    val data3: ListBuffer[(Long, Double)] = ListBuffer((1355270839,1.80225))
 
-    val data: List[(Long, Double)] =
-      List((1355270609,1.80215),
-        (1355270621,1.80185),
-        (1355270646,1.80195),
-        (1355270702,1.80225),
-        (1355270702,1.80215),
-        (1355270829,1.80235),
-        (1355270854,1.80205),
-        (1355270868,1.80225),
-        (1355271000,1.80245),
-        (1355271023,1.80285))
+    val dataConsolidated : (Long, Double, Int, Double, Double, Double) = (1355270580, 1.80215, 3, 5.40595, 1.80185, 1.80215)
+    val dataConsolidated1: (Long, Double, Int, Double, Double, Double) = (1355270640, 1.80225, 2, 3.60440, 1.80215, 1.80225)
+    val dataConsolidated2: (Long, Double, Int, Double, Double, Double) = (1355270700, 1.80235, 2, 3.60440, 1.80205, 1.80235)
+    val dataConsolidated3: (Long, Double, Int, Double, Double, Double) = (1355270820, 1.80225, 1, 1.80225, 1.80225, 1.80225)
 
-    val dataProcessed: List[(Long, Double, Int, Double, Double, Double)] =
-      List((1355270820, 1.80235, 3, 5.40665, 1.80205, 1.80235),
-        (1355271000, 1.80245, 2, 3.6053, 1.80245, 1.80285),
-        (1355270580, 1.80215, 2, 3.604, 1.80185, 1.80215),
-        (1355270640, 1.80195, 1, 1.80195, 1.80195, 1.80195),
-        (1355270700, 1.80225, 2, 3.6044, 1.80215, 1.80225))
-
-    Assert.assertEquals(dataProcessed, process(data))
+    Assert.assertEquals(dataConsolidated , consolidate(data))
+    Assert.assertEquals(dataConsolidated1, consolidate(data1))
+    Assert.assertEquals(dataConsolidated2, consolidate(data2))
+    Assert.assertEquals(dataConsolidated3, consolidate(data3))
 
   }
 
@@ -97,14 +84,6 @@ class ChallengeTest {
     Assert.assertEquals(recordExpected2, toOutput(record2))
     Assert.assertEquals(recordExpected3, toOutput(record3))
 
-  }
-
-  @Test
-  def getHeaderTest: Unit = {
-    val headerExpected = "T          S       N   RS         MinV    MaxV   " +
-      "\n--------------------------------------------------"
-
-    Assert.assertEquals(headerExpected, getHeader)
   }
 
 }
